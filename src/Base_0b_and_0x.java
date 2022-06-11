0c4ff69fea28fee0ea2fa83a6dd51d0241a5374d
ChengJiaxing <74436448+ChengJiaxing@users.noreply.github.com>
2022-06-11 11:07:48 +0800

    Initial commit

---
+++ src/Base_0b_and_0x.java
@@ -0,0 +1,19 @@
+public class Base_0b_and_0x {
+    public static void main(String [] args)
+    {
+//        int a=25;
+//        System.out.println(Integer.toBinaryString((a)));
+//        System.out.println(Integer.toHexString((a)));
+//        System.out.println(Long.toBinaryString(a));
+//        System.out.println(Long.toHexString(a));
+//
+//        int a=4;
+//        a=a>>2;
+//        System.out.println(a);
+//        a=a<<3;
+//        System.out.println(a);
+//        System.out.println(0.1f+0.1f);
+//        System.out.println(0.1f*0.1f);
+
+    }
+}
---
+++ src/_sum.java
@@ -0,0 +1,10 @@
+public class _sum {
+    public static int sum(int a,int b){
+        int c =a+b;
+        return c;
+    }
+    public static void main(String[] args){
+        int d=_sum.sum(1,2);
+        System.out.print(d);
+    }
+}
